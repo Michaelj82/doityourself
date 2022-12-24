da326d7d6002788675c2bc6e6dc43b43a201942f
Michael Johnson <mjj.yoyogi@gmail.com>
2022-12-23 23:56:13 -0500

    starting the task class

--- task-app/src/components/Overview.js
+++ task-app/src/components/Overview.js
@@ -1,1 +1,31 @@
 // create task object
+import React, {Component} from 'react';
+
+class Task extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            title: '',
+            dueDate: '',
+            description: '',
+
+        };
+    }
+
+    //example function
+    functionName(){
+        this.setState({
+            count: this.state.count + 1,
+        });
+    }
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
